da808d50174fcd4dd20d5e05dad3fe09ba90a96f
Leonardo Hildt <leonardohildt@gmail.com>
2017-11-24 16:51:46 -0300

    Fixed email to data to reply to owner request

--- web_app/sap_data.aspx.vb
+++ web_app/sap_data.aspx.vb
@@ -95,7 +95,7 @@ Partial Class Default2
             mail_dict.Add("{detail}", http_req_form_detail)
             mail_dict.Add("{app_link}", sysConfiguration.getSystemUrl)
             'for each admin in users.admins
-            mail_dict.Add("{reply_mail_link}", "mailto:" & userCommon.getAdminMail & "?subject=RQ#" & http_req_form_id.Trim() & "%20-%20To%20process%20your%20request%20additional%20info%20is%20needed&body=Dear%20Admin%2C%0D%0A%0D%0AHere%20is%20the%20info%20required%3A%0D%0A%0D%0A" & urlBody)
+            mail_dict.Add("{reply_mail_link}", "mailto:" & http_req_mail & "?subject=RQ#" & http_req_form_id.Trim() & "%20-%20To%20process%20your%20request%20additional%20info%20is%20needed&body=Dear%20" & userCommon.getNameByMail(http_req_mail) & "%2C%0D%0A%0D%0AHere%20is%20the%20info%20required%3A%0D%0A%0D%0A" & urlBody)
             'next
 			mail_dict.Add("{subject}", userCommon.getNameByMail(http_req_mail) & " is requesting information for AI#" & http_req_form_id.Trim())
 
