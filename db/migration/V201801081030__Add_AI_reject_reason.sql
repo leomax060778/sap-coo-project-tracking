06f0364045344ef35967fe30b5875b27f228eb17
Leonardo Hildt <leonardohildt@gmail.com>
2018-01-15 08:44:21 -0300

    Fixed migration file

--- db/migration/V201801081030__Add_AI_reject_reason.sql
+++ db/migration/V201801081030__Add_AI_reject_reason.sql
@@ -1,13 +1,3 @@
-
-
--- *************************************************************************************
--- Update schema version
---INSERT INTO SCHEMA_VERSION(VERSION, DESCRIPTION, SCRIPT) VALUES('V1.0.0-01', 'Add sent notification field to system', 'V201703161700__Add_sent_notification.sql');
-
-ALTER TABLE system ADD sent_notification DATE;
-
--- Set initial date
-UPDATE system SET sent_notification = '2017-03-01' WHERE id = 1;
-
+ALTER TABLE actionitems ADD reject_reason TEXT;
 
 COMMIT;
